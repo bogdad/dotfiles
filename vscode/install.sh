5599c48869d7441f22fba8918977d4f644b9dca9
Vladimir Shakhov <vladimir@spotify.com>
2017-06-23 14:49:23 +0200

    vscode link

---
+++ vscode/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+target="$HOME/Library/Application Support/Code/User/settings.json" original="$HOME/.dotfiles/vscode/settings.json"
+backup="$HOME/.dotfiles/vscode/settings.json.orig"
+if [[ ! -L "$target" ]]; then
+    echo "installing link $target for $original\n"
+    echo "backup is $bacup\n"
+    mv -- "$target" "$backup"
+    ln -s -- "$original" "$target"
+fi
