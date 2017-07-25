a2bd5b3bacfb6a09a6cfe4469caa3d23c687721b
Vladimir Shakhov <bogdad@gmail.com>
2017-07-25 21:49:03 +0200

    rust nightly

--- rust/install.sh
+++ rust/install.sh
@@ -5,3 +5,4 @@ rustup component add rust-src
 rustup component add rls
 rustup component add rust-src --toolchain nightly-2017-06-08-x86_64-apple-darwin
 rustup component add rls --toolchain nightly-2017-06-08-x86_64-apple-darwin
+rustup override add nightly-2017-06-08-x86_64-apple-darwin
