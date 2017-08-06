3e8129e6c35b69e063b65265b0980efb02f8e3fd
Vladimir Shakhov <vladimir@spotify.com>
2017-08-06 13:12:35 +0200

    traceflamegraph

--- bin/dtraceflamegraph.sh
+++ bin/dtraceflamegraph.sh
@@ -5,4 +5,4 @@ rm out.folded || TRUE
 dtrace -x ustackframes=100 -n "profile-99 /execname == \"${NAME}\" && arg1/ {
     @[ustack()] = count(); } tick-60s { exit(0); }" -o ./out.stacks
 stackcollapse.pl ./out.stacks > ./out.folded
-flamegraph.pl ./out.folded > ./${NAME//\//_}.svg
+flamegraph.pl -cp ./out.folded > ./${NAME//\//_}.svg
