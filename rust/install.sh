7ee4b84460a0fa9ef414c391a1a1055d3f1e2767
Vladimir Shakhov <vladimir@spotify.com>
2017-09-29 16:12:12 +0200

    rust update nightly

--- rust/install.sh
+++ rust/install.sh
@@ -1,8 +1,8 @@
 source ~/.cargo/env
-rustup override add nightly-2017-06-08-x86_64-apple-darwin
-rustup default nightly-2017-06-08-x86_64-apple-darwin
+rustup override add nightly-2017-09-07-x86_64-apple-darwin
+rustup default nightly-2017-09-07-x86_64-apple-darwin
 rustup component add rust-src
 rustup component add rls
-rustup component add rust-src --toolchain nightly-2017-06-08-x86_64-apple-darwin
-rustup component add rls --toolchain nightly-2017-06-08-x86_64-apple-darwin
-rustup override add nightly-2017-06-08-x86_64-apple-darwin
+rustup component add rust-src --toolchain nightly-2017-09-07-x86_64-apple-darwin
+rustup component add rls --toolchain nightly-2017-09-07-x86_64-apple-darwin
+rustup override add nightly-2017-09-07-x86_64-apple-darwin
