d1c76917f5691a2dd833b8fbd30b690253a209ec
Vladimir Shakhov <vladimir@spotify.com>
2018-05-01 11:12:12 +0200

    rr

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ end
 
 alias pssh='pssh -X -A -x "-q -t -t -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"'
 alias ssh='ssh -A'
+alias rr='reveal role'
 alias dr='disco role'
 alias gs='git status -s'
 alias ga='git add'
