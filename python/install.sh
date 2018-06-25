56666a4133df1b4f0e950a9e9fec39b5e0627b46
Vladimir Shakhov <vladimir@spotify.com>
2018-06-25 14:41:18 +0200

    python brew pydistutils to fix vendoring for joblib

--- python/install.sh
+++ python/install.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -e -x 
 target="$HOME/.pydistutils.cfg" original="$HOME/.dotfiles/python/.pydistutils.cfg"
 backup="$HOME/.dotfiles/python/.pydistutils.cfg.orig"
 if [[ ! -L "$target" ]]; then
