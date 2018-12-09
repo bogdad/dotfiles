6103957b76468239be518fc53785dba0628ec550
Vladimir Shakhov <vladimir@spotify.com>
2018-12-09 16:17:25 +0100

    sublime settings track

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,5 +1,45 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -ex -o pipefail
+
 target="/usr/local/bin/subl"
 if [[ ! -L "$target" ]]; then
 ln -s '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' "$target"
 fi
+
+
+replace_with_symlink() {
+	target=$1
+	original=$2
+
+	suff=".orig"
+	backup="$original$suff"
+
+    if [[ ! -f $original ]]; then
+        if [[ -f $target ]]; then
+	        cp "$target" "$original"
+        fi
+    fi
+
+    if [[ ! -L "$target" ]]; then
+        echo "installing link $target for $original\n"
+        echo "backup is $backup\n"
+        mv -- "$target" "$backup" || true
+        ln -s -- "$original" "$target"
+    fi
+}
+
+
+target="$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" 
+original="$HOME/.dotfiles/sublime/Preferences.sublime-settings"
+replace_with_symlink "$target" "$original"
+
+target="$HOME/Library/Application Support/Sublime Text 3/Packages/User/RustEnhanced.sublime-settings" 
+original="$HOME/.dotfiles/sublime/RustEnhanced.sublime-settings"
+replace_with_symlink "$target" "$original"
+
+target="$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings" 
+original="$HOME/.dotfiles/sublime/Package Control.sublime-settings"
+replace_with_symlink "$target" "$original"
+
+
