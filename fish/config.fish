21374669e44d9ce6603c5465737af64797c3ba6c
Vladimir Shakhov <bogdad@gmail.com>
2020-02-05 12:55:56 +0100

    alias + ongoing backup

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,8 @@ alias gd.='git diff -M --color-words="."'
 alias gd='git diff -M'
 alias gb='git branch'
 
+alias cmakecoreiossimulator='cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCORE_BUILD_HOST_TOOLS=ON -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/iphone-core.cmake -DIOS_PLATFORM=SIMULATOR'
+
 alias gcm='git checkout master'
 alias grv='git remote -v'
 alias gcamend='git commit --amend --no-edit'
@@ -44,8 +46,11 @@ export PATH="$PATH:/Users/vladimir/go/bin/"
 export PATH="$PATH:/Users/vladimir/.vulcan/cache/clang-format-9.0.0-393574-osx.tar.gz-3611c06add394d2e6a5fb195fd874c4d054bdb74/extracted/"
 export PATH="/usr/local/opt/ruby/bin:$PATH"
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
+export PATH="/usr/local/lib/ruby/gems/2.6.0/gems/xcpretty-0.3.0/bin/:$PATH"
+export PATH="$PATH:$HOME/.vulcan/cache/sdk-tools-darwin-4333796.zip-9b11aeda2adabaaebd127b9b6ed11de8e11e2844/extracted/platform-tools/"
 
 #export PATH="/usr/local/anaconda3/bin:$PATH"
+
 
 set PYTHONHOME "/usr/local/Cellar/python/2.7.13/Frameworks/Python.framework/Versions/2.7/"
 export env JAVA_HOME=(/usr/libexec/java_home -v 1.8)
@@ -53,6 +58,7 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 export env XDG_CONFIG_HOME="$HOME/.config"
 
+export NDK="/Users/vladimir/.vulcan/cache/sdk-tools-darwin-4333796.zip-9b11aeda2adabaaebd127b9b6ed11de8e11e2844/extracted/ndk/20.1.5948944"
 
 export env LDFLAGS="-L/usr/local/opt/openssl/lib:$LDFLAGS"
 export env CPPFLAGS="-I/usr/local/opt/openssl/include:$CPPFLAGS"
