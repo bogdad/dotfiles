fea7f00b19c4abaaff314474f606ffb9a8461472
Vladimir Shakhov <bogdad@gmail.com>
2025-11-08 16:37:35 +0100

    fixes: fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,15 +5,15 @@
 #fish_add_path (find $HOME/.sdkman/candidates/*/current/bin -maxdepth 0)
 
 function fish_user_key_bindings
-  bind \eB backward-word
-  bind \eF forward-word
+    bind \eB backward-word
+    bind \eF forward-word
 end
+
+source "$HOME/.cargo/env.fish"
 
 alias pssh='pssh -X -A -x "-q -t -t -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"'
 alias pscp='pscp -X -A -x "-q -t -t -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"'
 alias ssh='ssh -A'
-alias rr='reveal role'
-alias dr='disco role'
 alias gs='git status -s'
 alias ga='git add'
 alias gc='git commit'
@@ -30,7 +30,7 @@ alias gf='git fetch '
 alias gl='git log '
 alias cninja='echo \n\n\n\n\n\n\n\n\n\n; and ninja'
 
-alias cmakecoreiossimulator='cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCORE_BUILD_HOST_TOOLS=ON -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/iphone-core.cmake -DIOS_PLATFORM=SIMULATOR'
+#alias cmakecoreiossimulator='cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCORE_BUILD_HOST_TOOLS=ON -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/iphone-core.cmake -DIOS_PLATFORM=SIMULATOR'
 
 alias gcm='git checkout master'
 alias grv='git remote -v'
@@ -69,28 +69,44 @@ export PATH="/opt/homebrew/bin:$PATH"
 export PATH="$PATH:$HOME/google-cloud-sdk/bin"
 export PATH="$PATH:$HOME/.local/bin"
 
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home"
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+#export JAVA_HOME="/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home"
+set -g fish_user_paths /usr/local/sbin $fish_user_paths
 
 export env XDG_CONFIG_HOME="$HOME/.config"
 
 export env PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '$HOME/.dotfiles/fish/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '$HOME/.dotfiles/fish/google-cloud-sdk/path.fish.inc'; else; . '$HOME/.dotfiles/fish/google-cloud-sdk/path.fish.inc'; end; end
+if [ -f '$HOME/.dotfiles/fish/google-cloud-sdk/path.fish.inc' ]
+    if type source >/dev/null
+        source '$HOME/.dotfiles/fish/google-cloud-sdk/path.fish.inc'
+    else
+        . '$HOME/.dotfiles/fish/google-cloud-sdk/path.fish.inc'
+    end
+end
+
+export env GEMINI_API_KEY="AIzaSyCDXi09lPYuWJFgLuhds5bLZPSiytCtqtk"
 
 fish_add_path /opt/homebrew/sbin
-
+fish_add_path /opt/homebrew/opt/lld@19/bin
 fish_add_path $HOME/.nvm
 
+fish_add_path /opt/homebrew/Cellar/llvm/20.1.2/bin/
+
+fish_add_path $HOME/yandex-cloud/bin/yc
 
 export VCPKG_ROOT="$HOME/vcpkg"
-status --is-interactive; and source (jenv init -|psub)
+#status --is-interactive; and source (jenv init -|psub)
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" hook $argv | source
 # <<< conda initialize <<<
 
-export env FLYTECTL_CONFIG="$HOME/.flyte/config.yaml"
-export env JAVA_HOME="/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home"
+#export env FLYTECTL_CONFIG="$HOME/.flyte/config.yaml"
+#export env JAVA_HOME="/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home"
+
+alias claude="/Users/vladimir/.claude/local/claude"
+
+# opencode
+fish_add_path /Users/vladimir/.opencode/bin
